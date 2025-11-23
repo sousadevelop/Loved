9465c09fd5899f714913703bc29501c917288088
João Victor <92130316+sousadevelop@users.noreply.github.com>
2025-11-23 23:21:36 +0000

    a configuração do database.js está mais robusta para lidar com erros

--- infra/database.js
+++ infra/database.js
@@ -9,9 +9,15 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error("Database query error:", error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
